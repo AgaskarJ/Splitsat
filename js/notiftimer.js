a56b8bdfc637a4043d26ff3b4cc2bc08f7aa39de
Jay Agaskar <jayagaskar47@gmail.com>
2018-10-24 02:09:29 -0400

    javascript files

---
+++ js/notiftimer.js
@@ -0,0 +1,5 @@
+var timer = 0;
+function set_interval() {
+timer = setInterval("function()", 300000);
+// set to 10 minutes
+}
---
+++ js/reload.js
@@ -0,0 +1,18 @@
+var timer = 0;
+function set_interval() {
+timer = setInterval("oxne()", 3000);
+// set to 10 minutes
+}
+
+var timer2 = 0;
+function set_interval2() {
+timer2 = setInterval("toastit2()", 3000);
+// set to 10 minutes
+}
+
+
+
+
+ 
+
+ 
