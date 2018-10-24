2a6905a31e160651fed02c09e754fd8bcd44f6df
Jay Agaskar <jayagaskar47@gmail.com>
2018-10-24 02:05:57 -0400

    clicking on specific letters for filter

---
+++ Pages/letterclick.php
@@ -0,0 +1,214 @@
+<?php 
+
+function clicka() {
+	if ($_GET['search'] == A) {
+		echo "<b>A</b>";
+	} else {
+		echo "<a href =searchletter.php?search=A>A</a>";
+	}
+}
+
+function clickb() {
+	if ($_GET['search'] == B) {
+		echo "<b>B</b>";
+	} else {
+	echo "<a href =searchletter.php?search=B>B</a>";
+	}
+}
+
+
+function clickc() {
+	if ($_GET['search'] == C) {
+		echo "<b>C</b>";
+	} else {
+	echo "<a href =searchletter.php?search=C>C</a>";
+	}
+}
+
+function clickd() {
+	if ($_GET['search'] == D) {
+		echo "<b>D</b>";
+	} else {
+	echo "<a href =searchletter.php?search=D>D</a>";
+	}
+}
+
+function clicke() {
+	if ($_GET['search'] == E) {
+		echo "<b>E</b>";
+	} else {
+	echo "<a href =searchletter.php?search=E>E</a>";
+	}
+}
+
+function clickf() {
+	if ($_GET['search'] == F) {
+		echo "<b>F</b>";
+	} else {
+	echo "<a href =searchletter.php?search=F>F</a>";
+	}
+}
+
+function clickg() {
+	if ($_GET['search'] == G) {
+		echo "<b>G</b>";
+	} else {
+	echo "<a href =searchletter.php?search=G>G</a>";
+	}
+}
+
+function clickh() {
+	if ($_GET['search'] == H) {
+		echo "<b>H</b>";
+	} else {
+	echo "<a href =searchletter.php?search=H>H</a>";
+	}
+}
+
+function clicki() {
+	if ($_GET['search'] == I) {
+		echo "<b>I</b>";
+	} else {
+	echo "<a href =searchletter.php?search=I>I</a>";
+	}
+}
+
+function clickj() {
+	if ($_GET['search'] == J) {
+		echo "<b>J</b>";
+	} else {
+	echo "<a href =searchletter.php?search=J>J</a>";
+	}
+}
+
+function clickk() {
+	if ($_GET['search'] == K) {
+		echo "<b>K</b>";
+	} else {
+	echo "<a href =searchletter.php?search=K>K</a>";
+	}
+}
+
+function clickl() {
+	if ($_GET['search'] == L) {
+		echo "<b>L</b>";
+	} else {
+	echo "<a href =searchletter.php?search=L>L</a>";
+	}
+}
+
+
+function clickm() {
+	if ($_GET['search'] == M) {
+		echo "<b>M</b>";
+	} else {
+	echo "<a href =searchletter.php?search=M>M</a>";
+	}
+}
+
+
+function clickn() {
+	if ($_GET['search'] == N) {
+		echo "<b>N</b>";
+	} else {
+	echo "<a href =searchletter.php?search=N>N</a>";
+	}
+}
+
+function clicko() {
+	if ($_GET['search'] == O) {
+		echo "<b>O</b>";
+	} else {
+	echo "<a href =searchletter.php?search=O>O</a>";
+	}
+}
+
+function clickp() {
+	if ($_GET['search'] == P) {
+		echo "<b>P</b>";
+	} else {
+	echo "<a href =searchletter.php?search=P>P</a>";
+	}
+}
+
+function clickq() {
+	if ($_GET['search'] == Q) {
+		echo "<b>Q</b>";
+	} else {
+	echo "<a href =searchletter.php?search=Q>Q</a>";
+	}
+}
+
+function clickr() {
+	if ($_GET['search'] == R) {
+		echo "<b>R</b>";
+	} else {
+	echo "<a href =searchletter.php?search=R>R</a>";
+	}
+}
+
+function clicks() {
+	if ($_GET['search'] == S) {
+		echo "<b>S</b>";
+	} else {
+	echo "<a href =searchletter.php?search=S>S</a>";
+	}
+}
+
+function clickt() {
+	if ($_GET['search'] == T) {
+		echo "<b>T</b>";
+	} else {
+	echo "<a href =searchletter.php?search=T>T</a>";
+	}
+}
+
+function clicku() {
+	if ($_GET['search'] == U) {
+		echo "<b>U</b>";
+	} else {
+	echo "<a href =searchletter.php?search=U>U</a>";
+	}
+}
+
+function clickv() {
+	if ($_GET['search'] == V) {
+		echo "<b>V</b>";
+	} else {
+	echo "<a href =searchletter.php?search=V>V</a>";
+	}
+}
+
+function clickw() {
+	if ($_GET['search'] == W) {
+		echo "<b>W</b>";
+	} else {
+	echo "<a href =searchletter.php?search=W>W</a>";
+	}
+}
+
+function clickx() {
+	if ($_GET['search'] == X) {
+		echo "<b>X</b>";
+	} else {
+	echo "<a href =searchletter.php?search=X>X</a>";
+	}
+}
+
+function clicky() {
+	if ($_GET['search'] == L) {
+		echo "<b>Y</b>";
+	} else {
+	echo "<a href =searchletter.php?search=Y>Y</a>";
+	}
+}
+
+function clickz() {
+	if ($_GET['search'] == Z) {
+		echo "<b>Z</b>";
+	} else {
+	echo "<a href =searchletter.php?search=Z>Z</a>";
+	}
+}
+
+?> 
